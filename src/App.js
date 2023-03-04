a5a6a88e906c9a5210fcf2be7c307b101b20a9aa
S1xthRose <Maximovcontacts@yandex.ru>
2023-03-04 04:26:13 +0400

    <Created items and items list>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
-function App() {
-  return (
-   <div>Hi</div>
-  );
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Профессиональный мяч',
+          img: 'ball.jpeg',
+          desc: 'One of the best professional balls used in world football.',
+          category: 'balls',
+          price: '59.99'
+        },
+        {
+          id: 2,
+          title: 'Бутсы',
+          img: 'boots.jpeg',
+          desc: 'Speed, lightness and conciseness. Stand out - easy.',
+          category: 'Boots',
+          price: '99.99'
+        },
+        {
+          id: 3,
+          title: 'Гетры',
+          img: 'get.jpeg',
+          desc: 'The best quality of materials. Everything you need for a result.',
+          category: 'gaiters',
+          price: '29.99'
+        },
+        {
+          id: 4,
+          title: 'Лонгслив',
+          img: 'long.jpeg',
+          desc: 'Invulnerability from defeats. Play like a storm.',
+          category: 'longsleeve',
+          price: '79.99'
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Football Land</span>
+            <ul className='nav'>
+                <li>О нас</li>
+                <li>Контакты</li>
+                <li>Кабинет</li>
+            </ul>
+        </div>
+        <div className='presentation'>
+
+        </div>
+    </header>
+  )
+}
